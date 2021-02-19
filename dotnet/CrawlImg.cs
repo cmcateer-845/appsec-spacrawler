a2dc328b5e9630428cc51c919d7cc6f3145efcef
Conor McAteer <51858111+cmcateer-845@users.noreply.github.com>
2021-02-19 23:07:58 +0000

    Tidied some dotnet code.

---
+++ dotnet/CrawlImg.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace SpaCrawler
+{
+    internal class CrawlImg
+    {
+        public Bitmap BitMap { get; }
+        public string FilePath { get; }
+
+        public CrawlImg()
+        {
+            BitMap = null;
+            FilePath = "";
+        }
+        public CrawlImg(Bitmap bitMap, string filePath)
+        {
+            BitMap = bitMap;
+            FilePath = filePath;
+        }
+    }
+}
--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -19,7 +19,6 @@ namespace SpaCrawler
             Console.WriteLine(string.Format("\t{0,-30}\t{1}", "--screenShots", "Take a screenshot of each page visited during crawl."));
             Console.WriteLine(string.Format("\t{0,-30}\t{1}", "--dumpDom", "Dump the DOM of each page visited during crawl."));
             Console.WriteLine(string.Format("\t{0,-30}\t{1}", "--headless", "Use headless browser."));
-            Console.WriteLine(string.Format("\t{0,-30}\t{1}", "--outputDirectory <directory>", "Output directory of crawl results."));
             Console.WriteLine(string.Format("\t{0,-30}\t{1}", "--help", "Show this help page."));
 
             Environment.Exit(0);
@@ -27,7 +26,6 @@ namespace SpaCrawler
         static async Task Main(string[] args)
         {
             uint depth = 0;
-            string outputDirectory = "";
             string seedUrl = "";
             bool screenShots = false;
             bool dumpDom = false;
@@ -67,16 +65,6 @@ namespace SpaCrawler
                         }
                         break;
 
-                    case "--outputDirectory":
-                        i++;
-                        if (string.IsNullOrEmpty(args[i]) ||
-                            args[i].StartsWith("--"))
-                        {
-                            ShowHelp("--outputDirectory requires a valid directory as an argument.");
-                        }
-                        outputDirectory = args[i];
-                        break;
-
                     case "--screenShots":
                         screenShots = true;
                         break;
@@ -105,16 +93,10 @@ namespace SpaCrawler
                 ShowHelp("--depth requires a valid unsigned integer as an argument.");
                 Environment.Exit(1);
             }
-            else if(outputDirectory.Length == 0 || !Directory.Exists(outputDirectory))
-            {
-                ShowHelp("--outputDirectory requires a valid directory as an argument.");
-                Environment.Exit(1);
-            }
 
             Console.WriteLine("Command line arguments:");
             Console.WriteLine(string.Format("seedUrl={0}", seedUrl));
             Console.WriteLine(string.Format("depth={0}", depth));
-            Console.WriteLine(string.Format("outputDirectory={0}", outputDirectory));
             Console.WriteLine(string.Format("headless={0}", headless ? "true" : "false"));
             Console.WriteLine(string.Format("screenShots={0}", screenShots ? "true" : "false"));
             Console.WriteLine(string.Format("dumpDom={0}", dumpDom ? "true" : "false"));
@@ -122,13 +104,10 @@ namespace SpaCrawler
             try
             {
                 Crawler crawler = new Crawler();
-                CrawlSettings crawlSettings = new CrawlSettings(new Uri(seedUrl), outputDirectory, depth, headless, screenShots, dumpDom);
+                CrawlSettings crawlSettings = new CrawlSettings(new Uri(seedUrl), depth, headless, screenShots, dumpDom);
                 await crawler.RunAsync(crawlSettings);
 
-                Console.WriteLine(
-                    string.Format("Success: Crawl of {0} completed successfully - Report written to {1}",
-                    seedUrl,
-                    outputDirectory));
+                Console.WriteLine(string.Format("Success: Crawl of {0} completed successfully.", seedUrl));
             }
             catch (ArgumentException e)
             {
